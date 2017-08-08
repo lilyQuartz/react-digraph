ed7ea53a274b31a7cba3a64f622928f6c72108e2
Lily Quartz <lilyquartz@gmail.com>
2017-08-08 13:48:21 -0700

    Update graph-config.js
    Pasted the default config example in.

--- src/examples/graph-config.js
+++ src/examples/graph-config.js
@@ -22,6 +22,7 @@
   Example config for GraphView component
 */
 import React from 'react';
+import GraphView from 'react-digraph'
 
 const EmptyShape = (
   <symbol viewBox="0 0 100 100" id="empty">
@@ -82,4 +83,81 @@ export default {
       shape: SpecialEdgeShape
     }
   }
+}
+
+const GraphConfig =  {
+  NodeTypes: {
+    empty: {
+      typeText: "None",
+      shapeId: "#empty",
+      shape: (
+        <symbol viewBox="0 0 100 100" id="empty" key="0">
+          <circle cx="50" cy="50" r="45"></circle>
+        </symbol>
+      )
+    }
+  }, 
+  NodeSubtypes: {}, 
+  EdgeTypes: {
+    emptyEdge: {
+      shapeId: "#emptyEdge",
+      shape: (
+        <symbol viewBox="0 0 50 50" id="emptyEdge" key="0">
+          <circle cx="25" cy="25" r="8" fill="currentColor"> </circle>
+        </symbol>
+      )
+    }
+  }
+}
+
+const EMPTY_TYPE = "empty"  // Text on empty nodes is positioned differently 
+const NODE_KEY = "id"       // Allows D3 to correctly update DOM
+
+class Graph extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      graph: sample,
+      selected: {}
+    }
+  }
+
+  /* Define custom graph editing methods here */
+
+  render() {
+    const nodes = this.state.graph.nodes;
+    const edges = this.state.graph.edges;
+    const selected = this.state.selected;
+
+    const NodeTypes = GraphConfig.NodeTypes;
+    const NodeSubtypes = GraphConfig.NodeSubtypes;
+    const EdgeTypes = GraphConfig.EdgeTypes;
+
+    return (
+      <div id='graph' style={styles.graph}>
+      
+        <GraphView  ref='GraphView'
+                    nodeKey={NODE_KEY}
+                    emptyType={EMPTY_TYPE}
+                    nodes={nodes}
+                    edges={edges}
+                    selected={selected}
+                    nodeTypes={NodeTypes}
+                    nodeSubtypes={NodeSubtypes}
+                    edgeTypes={EdgeTypes}
+                    getViewNode={this.getViewNode}
+                    onSelectNode={this.onSelectNode}
+                    onCreateNode={this.onCreateNode}
+                    onUpdateNode={this.onUpdateNode}
+                    onDeleteNode={this.onDeleteNode}
+                    onSelectEdge={this.onSelectEdge}
+                    onCreateEdge={this.onCreateEdge}
+                    onSwapEdge={this.onSwapEdge}
+                    onDeleteEdge={this.onDeleteEdge}/>
+      </div>
+    );
+  }
+
 }
